0425104e21b2dda6f3c92e10b024702323b9f418
liqiang <qiangwushuang@126.com>
2020-03-28 23:46:16 +0800

    WIP:获取CAA版本

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+var fs = require('fs')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,16 +9,9 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('CAA二次开发插件');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let buildModule = vscode.commands.registerCommand('extension.buildModule', (uri) => {
-		var FilePath = uri.path;
+		var FilePath = uri.fsPath;
 		var pos = FilePath.indexOf('.m')
 		if(pos > 0){
 			var ModulePath = FilePath.substring(0,pos+2)
@@ -26,9 +19,16 @@ function activate(context) {
 
 			//编译Module
 			//Step1，TCK INIT
-			
+			var levelFile = FindFile(FilePath,"CATIAV5Level.lvl");
+			if(levelFile.length > 0){
+				var caalvl = ReadCAALevel(levelFile);
+				console.log("当前CAA版本为："+caalvl);
 
 
+			}
+			else{
+				vscode.window.showInformationMessage('获取当前CAA版本失败，请检查是否为CAA工程，以及文件工程下是否存在CATIAV5Level.lvl文件');
+			}
 
 		}
 		else{
@@ -54,4 +54,85 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+
+/**
+ * @param {any} uri
+ */
+function CheckTCKLevel(uri)
+{
+	
+}
+
+
+/**
+ * @param {string} lvlFile
+ */
+function ReadCAALevel(lvlFile)
+{
+	var lvl = "";
+	var exist = fs.existsSync(lvlFile);
+	if(exist.valueOf()){
+		/* fs.readFileSync(lvlFile){
+			if(err){
+				console.log(err);
+			}
+
+			var pos = data.lastIndexOf("#define");
+			if(pos >= 0){
+				var pos1 = data.indexOf("CATIA",pos);
+				var pos2 = data.indexOf("\n",pos1);
+				lvl = data.substring(pos1,pos2);
+			}
+
+		}); */
+
+		var fcont = fs.readFileSync(lvlFile);
+		var data = fcont.toString();
+		var pos = data.lastIndexOf("#define");
+			if(pos >= 0){
+				var pos1 = data.indexOf("CATIA",pos);
+				var pos2 = data.indexOf("\n",pos1);
+				lvl = data.substring(pos1,pos2);
+			}
+	}
+	else{
+		console.log(lvlFile+"文件不存在");
+	}
+	return lvl;
+}
+
+/**
+ * @param {string} path
+ * @param {string} file
+ */
+function FindFile(path,file)
+{
+	var refile = "";
+	var openfile = fs.lstatSync(path);
+	if(openfile.isDirectory()){
+		var files = fs.readdirSync(path);
+		var pos =  files.indexOf(file);
+		if(pos >= 0){
+			refile = path+"\\"+file;
+		}
+		else
+		{
+			var pos = path.lastIndexOf("\\");
+			if(pos > 1)
+			{
+				refile = FindFile(path.substring(0,pos),file);
+			}
+			
+		}		
+	}
+	else{
+		var pos = path.lastIndexOf("\\");
+		if(pos > 1)
+		{
+			refile = FindFile(path.substring(0,pos),file);
+		}		
+	}	
+	return refile;
 }
